1529c6db2cb9cafe5685344c7546aae378588245
IlyaBOT <isarbeu@gmail.com>
2023-02-21 21:20:37 +0300

    Upload

---
+++ dist/plugin.js
@@ -0,0 +1,6 @@
+exports.version = 1.0
+exports.description = "Mini video player by IlyaBOT"
+exports.apiRequired = 5
+exports.repo = "IlyaBOT/video-player"
+exports.frontend_css = 'style.css'
+exports.frontend_js = 'main.js'
---
+++ dist/public/main.js
@@ -0,0 +1,23 @@
+HFS.onEvent('afterEntryName', ({ entry }) =>
+    /\.(mp4|avi|mkv|mov|wmv)$/.test(entry.n) &&
+        `<button class='play-button' onclick='videoplay("${entry.n}")' />`)
+
+function videoplay(name = '') {
+    const root = document.getElementById('video-player')
+    root.style.display = name ? 'flex' : ''
+    const video = root.querySelector('video')
+    video.src = name
+    if (name) video.play()
+    else video.pause()
+    root.querySelector('#player-title').innerText = name
+}
+
+HFS.onEvent('afterMenuBar', () => `
+    <div id='video-player'>
+        <video class="video-element" controls></video>
+        <div>
+            <span id='player-title' class='track-title'></span>
+            <button onclick="videoplay()">✕</button>
+        </div>
+    </div>
+`)
