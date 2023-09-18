ef8aeeaa7014cbbeecebde3f55f24c21c493b31e
IlyaBOT <36928846+IlyaBOT@users.noreply.github.com>
2023-09-18 17:01:11 +0300

    Plugin outdated

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,5 +1,5 @@
 exports.version = 1.4
-exports.description = "Mini video player by IlyaBOT"
+exports.description = "Mini video player by IlyaBOT [OUTDATED]"
 exports.apiRequired = 5
 exports.repo = "IlyaBOT/video-player"
 exports.frontend_css = 'style.css'
